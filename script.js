acde05a7e4abc0b2331ce045b0a65c08758fcbd6
accio-f3 <acciof3@gmail.com>
2023-08-17 20:59:44 +0530

    9th class - 1st half

---
+++ script.js
@@ -0,0 +1,14 @@
+const joinBtn = document.getElementById('join-btn');
+const usernameInput = document.getElementById('username-input');
+const form = document.querySelector('.form-username');
+const chatRoomContainer = document.querySelector('.chatroom-container');
+let username='';
+
+joinBtn.addEventListener('click',event=>{
+    event.preventDefault();
+    username=usernameInput.value;
+    if(username!==''){
+        form.style.display='none';
+        chatRoomContainer.style.display='block';
+    }
+})
